3b8a724a53cea2c9e00df928347c39abeb2d149e
joshseve <josh.seve@gmail.com>
2017-06-28 09:39:20 -0500

    Corrigiendo la version para tablets, y adaptando para celulares

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 var api = {
-  url:'http://laboratoria.cuadra.co:9339/api/v1/students/'
+	url: "//laboratoria.cuadra.co/api/v1/students/"
 };
 
-var crearElementos = function(datosAlumna){
-  var $alumna=$('<li class="alumna"/>').text( "  "+datosAlumna.name + " " + datosAlumna.pLastName + "  " + datosAlumna.mLastName)
-  var $checkbox = $('<input type="checkbox" class="alumnaCheck"/>');
-  $alumna.prepend($checkbox);
-  var $label = $('<label/>').append($alumna)
-  $("#listadoAlumnas").append($label);
+var plantillaLista = '<li>' +
+		'__nombre__' + ' ' +
+		'<input type="checkbox">' +
+		'</li>';
+
+var cargarPagina = function () {
+	cargarLista();
 };
 
-$(document).ready(function(){
-  $.getJSON(api.url,function(alumnas){
-    alumnas.forEach(function(alumna){
-        crearElementos(alumna);
-    });
-  });
-});
+var cargarLista = function () {
+	$.getJSON(api.url, function (elementos) {
+		elementos.forEach(mostrarElementos)
+	});
+};
+
+var mostrarElementos = function (elemento) {
+	var plantillaFinal = " ";
+
+	var nombre = elemento.name;
+
+	plantillaFinal += plantillaLista
+		.replace('__nombre__', nombre);
+
+
+	$("#lista").append(plantillaFinal);
+};
+
+$(document).ready(cargarPagina);
